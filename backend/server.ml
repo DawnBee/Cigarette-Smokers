eeaa2d2f98dc4b9376531e0a1060f0f517622505
DawnBee <52739140+DawnBee@users.noreply.github.com>
2024-09-17 10:38:53 +0700

    Update server.ml

--- backend/server.ml
+++ backend/server.ml
@@ -4,9 +4,13 @@ let () =
   @@ Dream.logger
   @@ Dream.router [
     Dream.get "/" (fun _ -> Dream.html "<h1>Cigarette Smokers!</h1>");
-    Dream.get "/run-smokers" (fun _ ->
+    Dream.get "/api/simulation" (fun _ ->
       (* Trigger the smokers simulation *)
       Smokers.run_smokers_simulation ();
-      Dream.html "Smokers simulation is running!"
+
+      (* Create a JSON response manually *)
+      let response = `Assoc [("message", `String "Simulation started")] in
+      let response_string = Yojson.Safe.to_string response in
+      Dream.json response_string
     );
   ]
