55cc3a8a2fc027264e5d3412bb598bcd12e48345
DawnBee <52739140+DawnBee@users.noreply.github.com>
2024-09-17 10:17:50 +0700

    Added basic routing to server.ml

--- backend/server.ml
+++ backend/server.ml
@@ -1,5 +1,12 @@
 (* server.ml *)
 let () =
-  Dream.run (fun _ ->
-    Smokers.run_smokers_simulation ();  (* Call the smoker simulation *)
-    Dream.html "Avril Lavigne Rocks!!!")
+  Dream.run
+  @@ Dream.logger
+  @@ Dream.router [
+    Dream.get "/" (fun _ -> Dream.html "<h1>Cigarette Smokers!</h1>");
+    Dream.get "/run-smokers" (fun _ ->
+      (* Trigger the smokers simulation *)
+      Smokers.run_smokers_simulation ();
+      Dream.html "Smokers simulation is running!"
+    );
+  ]
